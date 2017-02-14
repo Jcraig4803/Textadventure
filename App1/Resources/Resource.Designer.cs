48f6e9eeee258649da3ad07acf8dd5cd1b8cf9cb
jcraig4803 <jcraig4803@WS-AC-CTD-37003>
2017-02-14 18:56:55 -0500

    blahblahblah

--- App1/Resources/Resource.Designer.cs
+++ App1/Resources/Resource.Designer.cs
@@ -57,6 +57,31 @@ namespace App1
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int charName = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int chatNext = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int chatbox = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
